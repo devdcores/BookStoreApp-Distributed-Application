d87c45124404ac9f82d2692ac33eb1dc67de24a1
Devaraj Reddy <devaraj.reddy@logmein.com>
2019-05-14 12:54:05 -0700

    Checkin Eureka Discovery Project

---
+++ bookstore-eureka-discovery-service/src/main/java/com/devd/spring/bookstoreeurekadiscoveryservice/BookstoreEurekaDiscoveryServiceApplication.java
@@ -0,0 +1,19 @@
+package com.devd.spring.bookstoreeurekadiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: Devaraj Reddy,
+ * Date : 2019-05-14 12:39
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class BookstoreEurekaDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreEurekaDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ bookstore-eureka-discovery-service/src/test/java/com/devd/spring/bookstoreeurekadiscoveryservice/BookstoreEurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.devd.spring.bookstoreeurekadiscoveryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author: Devaraj Reddy,
+ * Date : 2019-05-14 12:39
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookstoreEurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
