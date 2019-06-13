730cca4284763c88fb1fd79de22b6fef72ed357e
Devaraj Reddy <devaraj.reddy@logmein.com>
2019-06-13 11:43:08 -0700

    Order Service Project initial commit

---
+++ bookstore-order-service/src/main/java/com/devd/spring/bookstoreorderservice/BookstoreOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devd.spring.bookstoreorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreOrderServiceApplication.class, args);
+	}
+
+}
