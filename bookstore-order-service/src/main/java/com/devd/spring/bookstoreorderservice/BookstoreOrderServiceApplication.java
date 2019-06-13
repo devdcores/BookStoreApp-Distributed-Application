521f0945b55b077c5f1521123b5200f18594015c
Devaraj Reddy <devaraj.reddy@logmein.com>
2019-06-13 12:02:20 -0700

    add dependencies and properties to order service

--- bookstore-order-service/src/main/java/com/devd/spring/bookstoreorderservice/BookstoreOrderServiceApplication.java
+++ bookstore-order-service/src/main/java/com/devd/spring/bookstoreorderservice/BookstoreOrderServiceApplication.java
@@ -3,6 +3,10 @@ package com.devd.spring.bookstoreorderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author: Devaraj Reddy,
+ * Date : 2019-13-06
+ */
 @SpringBootApplication
 public class BookstoreOrderServiceApplication {
 
