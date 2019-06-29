eb74f6beaa8b677f3a2e68e0cb5d89abd76fcd80
Devaraj Reddy <devaraj.reddy@logmein.com>
2019-06-29 13:57:03 -0700

    Bookstore commons library, which contains common code needed for all microservices

---
+++ bookstore-order-service/src/main/java/com/devd/spring/bookstoreorderservice/config/ResourceServerConfig.java
@@ -0,0 +1,38 @@
+package com.devd.spring.bookstorecatalogservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private ResourceServerTokenServices tokenServices;
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("web").tokenServices(tokenServices);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .headers()
+                .frameOptions()
+                .disable()
+                .and()
+                .requestMatchers()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/**", "/api-docs/**", "/h2-console/**", "/signin").permitAll()
+                .antMatchers(HttpMethod.POST, "/oauth/token").permitAll()
+                .antMatchers(HttpMethod.GET, "/product**/**").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+}
