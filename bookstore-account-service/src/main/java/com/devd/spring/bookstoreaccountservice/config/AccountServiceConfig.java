4f65d8a6ebf5847d6a2bd575c79eb774365f929d
Devaraj Reddy <Devaraj.Reddy@logmein.com>
2019-08-29 03:09:55 -0700

    Account Service Refactor Configuration.

---
+++ bookstore-account-service/src/main/java/com/devd/spring/bookstoreaccountservice/config/AccountServiceConfig.java
@@ -0,0 +1,12 @@
+package com.devd.spring.bookstoreaccountservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Devaraj Reddy,
+ * Date : 2019-07-14
+ */
+@Configuration
+public class AccountServiceConfig {
+
+}
