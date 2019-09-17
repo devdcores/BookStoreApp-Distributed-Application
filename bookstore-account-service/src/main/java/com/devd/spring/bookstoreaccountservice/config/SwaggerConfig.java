a98bb39ae4bdbd085d5e09db0a17b22d2b6dedc9
Devaraj Reddy <Devaraj.Reddy@logmein.com>
2019-09-17 00:14:51 -0700

    Swagger fix in account service, default profile is h2, so no need to setup db.

--- bookstore-account-service/src/main/java/com/devd/spring/bookstoreaccountservice/config/SwaggerConfig.java
+++ bookstore-account-service/src/main/java/com/devd/spring/bookstoreaccountservice/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.devd.spring.bookstoreaccountservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -12,6 +13,7 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -21,6 +23,8 @@ import java.util.List;
  * @author: Devaraj Reddy,
  * Date : 2019-08-29
  */
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     
     @Autowired
